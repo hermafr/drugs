52cee178c9e5d5b1834d9527ec889ef68828d329
hermafr <hertelm@informatik.uni-freiburg.de>
2017-05-07 12:55:11 +0200

    training only on drugs without spaces, evaluation with false positives, confusion matrix, crossvalidation

--- multiclass_ngram_naive_bayes.py
+++ multiclass_ngram_naive_bayes.py
@@ -3,6 +3,8 @@ from pos_tagging import PosTagger, TaggedWord
 from feature_computation import n_grams, starts_with_uppercase
 from corpus_reader import read_dataset
 from numpy.random import choice
+from k_fold import k_folds
+import numpy as np
 
 VERBOSE = False
 
@@ -20,6 +22,8 @@ class MulticlassPosNgramNaiveBayes:
         self.nb.register_feature(self.word_length_feature_name)
         self.first_letter_uppercase_feature_name = "first_letter_uppercase"
         self.nb.register_feature(self.first_letter_uppercase_feature_name)
+        self.last_letter_s_feature_name = "last_letter_s"
+        self.nb.register_feature(self.last_letter_s_feature_name)
         # lemmas:
         self.lemma_before1_feature_name = "lemma-1"
         self.lemma_before2_feature_name = "lemma-2"
@@ -69,6 +73,8 @@ class MulticlassPosNgramNaiveBayes:
             features.append((self.word_length_feature_name, len(tagged_word.word)))
             # uppercase:
             features.append((self.first_letter_uppercase_feature_name, starts_with_uppercase(tagged_word.word)))
+            # last letter s:
+            features.append((self.last_letter_s_feature_name, tagged_word.word[-1] == 's'))
             #lemmas:
             features.append((self.lemma_before2_feature_name,
                              tagged_words[i - 2].lemma if (i - 2) > 0 else "#"))
@@ -105,6 +111,8 @@ class MulticlassPosNgramNaiveBayes:
                 for o in e.char_offset:
                     if self.intervals_intersect(tw.span, o):
                         label = e.type
+                #if [tw.span] == e.char_offset:
+                #    label = e.type
             labels.append(label)
         return labels
     
@@ -152,63 +160,113 @@ def get_gt_prediction_pairs(tagged_words, entities, labels):
     # get ground truth of predicted words
     for i in range(n_words):
         if labels[i] != "none":
+            entity_found = False
             span = [tagged_words[i].span]
             for j in range(n_entities):
                 if span == entities[j].char_offset:
                     pairs.append((tagged_words[i].word, entities[j].type, labels[i]))
                     entities_classified_as[j] = labels[i]
+                    entity_found = True
+                    break
+            if not entity_found:
+                pairs.append((tagged_words[i].word, "none", labels[i]))
     # get missing predictions
     for i in range(n_entities):
         if entities_classified_as[i] == "none":
             pairs.append((entities[i].text, entities[i].type, entities_classified_as[i]))
     return pairs
 
+def confusion_matrix(m, pairs):
+    for p in pairs:
+        truth = p[1]
+        pred = p[2]
+        if truth not in m:
+            m[truth] = {}
+        if pred not in m[truth]:
+            m[truth][pred] = 0
+        m[truth][pred] = m[truth][pred] + 1
+
+def print_confusion_matrix(m):
+    for truth in sorted(m):
+        for pred in sorted(m[truth]):
+            print(truth, pred, m[truth][pred])
+
 if __name__ == "__main__":
     data = read_dataset()
     n_docs = len(data)
-    train_amount = 0.7
-    train_ids = choice(n_docs, int(train_amount * n_docs), replace=False)
-    test_ids = [i for i in range(n_docs) if i not in train_ids]
-    training = [data[i] for i in train_ids]
-    test = [data[i] for i in test_ids]
     
-    print("%i training documents" % len(training))
-    print("%i test documents" % len(test))
+    n_folds = 10
+    folds = k_folds(n_docs, n_folds)
     
-    nb = MulticlassPosNgramNaiveBayes()
-    nb.train(training)
+    cv_results = {}
     
-    counters = {}
-    for c in nb.classes:
-        if c != "none":
+    for i_f in range(n_folds):
+        print("*" * 20)
+        print("FOLD %i" % i_f)
+        
+        train_ids = folds[i_f][0]
+        test_ids = folds[i_f][1]
+        training = [data[i] for i in train_ids]
+        test = [data[i] for i in test_ids]
+        
+        print("%i training documents" % len(training))
+        print("%i test documents" % len(test))
+        
+        nb = MulticlassPosNgramNaiveBayes()
+        nb.train(training)
+        
+        counters = {}
+        for c in nb.classes:
             counters[c] = [0, 0, 0]  # true positives, false positive, false negatives
-    
-    for doc in test:
-        for sentence in doc.sentences:
-            tagged_words = nb.tagger.pos_tag(sentence.text)
-            labels = nb.classify_tagged_words(tagged_words)
-            gt_pred_pairs = get_gt_prediction_pairs(tagged_words, sentence.entities, labels)
+        
+        conf_matrix = {}
+        
+        for doc in test:
+            for sentence in doc.sentences:
+                tagged_words = nb.tagger.pos_tag(sentence.text)
+                labels = nb.classify_tagged_words(tagged_words)
+                gt_pred_pairs = get_gt_prediction_pairs(tagged_words, sentence.entities, labels)
+                
+                if VERBOSE:
+                    print(sentence.text)
+                    print(gt_pred_pairs)
+                    print("")
+                
+                for pair in gt_pred_pairs:
+                    truth = pair[1]
+                    pred = pair[2]
+                    if pred == truth:  # correct prediction
+                        counters[pred][0] = counters[pred][0] + 1
+                    elif pred == "none":  # false negative
+                        counters[truth][2] = counters[truth][2] + 1
+                    else:  # wrong prediction
+                        counters[pred][1] = counters[pred][1] + 1  # false positive of predicted class
+                        counters[truth][2] = counters[truth][2] + 1  # false negative of true class
+                    
+                    #if truth != pred:
+                    #    print(pair)
+        
+                confusion_matrix(conf_matrix, gt_pred_pairs)
+        #print_confusion_matrix(conf_matrix)
+        
+        for c in sorted(counters):
+            true_positives = counters[c][0]
+            false_positives = counters[c][1]
+            false_negatives = counters[c][2]
+            precision = true_positives / (true_positives + false_positives) if (true_positives + false_positives) > 0 else 0
+            recall = true_positives / (true_positives + false_negatives) if (true_positives + false_negatives) > 0 else 0
+            f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+            print("%s: %s, p=%f, r=%f, f=%f" % (c, str(counters[c]), precision, recall, f1))
             
-            if VERBOSE:
-                print(sentence.text)
-                print(gt_pred_pairs)
-                print("")
-            
-            for pair in gt_pred_pairs:
-                truth = pair[1]
-                pred = pair[2]
-                if pred == truth:  # correct prediction
-                    counters[pred][0] = counters[pred][0] + 1
-                elif pred == "none":  # false negative
-                    counters[truth][2] = counters[truth][2] + 1
-                else:  # wrong prediction
-                    counters[pred][1] = counters[pred][1] + 1  # false positive of predicted class
-                    counters[truth][2] = counters[truth][2] + 1  # false negative of true class
+            if c not in cv_results:
+                cv_results[c] = [[],[],[]]
+            cv_results[c][0].append(precision)
+            cv_results[c][1].append(recall)
+            cv_results[c][2].append(f1)
     
-    for c in counters:
-        true_positives = counters[c][0]
-        false_positives = counters[c][1]
-        false_negatives = counters[c][2]
-        precision = true_positives / (true_positives + false_positives)
-        recall = true_positives / (true_positives + false_negatives)
-        print("%s: %s, p=%f, r=%f" % (c, str(counters[c]), precision, recall))
+    print("*" * 20)
+    for c in sorted(cv_results):
+        print("%s: p=%f, r=%f, f=%f" % (c,
+                                        np.mean(cv_results[c][0]),
+                                        np.mean(cv_results[c][1]),
+                                        np.mean(cv_results[c][2])))
