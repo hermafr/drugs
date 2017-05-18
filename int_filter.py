2534b5b2c5be077c94f24ee274a9c170c278eabc
hermafr <hertelm@informatik.uni-freiburg.de>
2017-05-18 13:08:44 +0200

    cleaning up directory

--- int_filter.py
+++
@@ -1,7 +0,0 @@
-from corpus_reader import read_dataset
-
-data = read_dataset
-for doc in data:
-    for sentence in doc.sentences:
-        for pair in sentence.pairs:
-            if 
--- random_seed_test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-np.random.seed(42)
-
-for i in range(10):
-    print(np.random.random())
